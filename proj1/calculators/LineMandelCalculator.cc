629524c2874a6fde366cc19ecd413779a1f888ca
Ondrej Lukasek <ondrej@lukasek.cz>
2024-10-21 22:17:06 +0200

    fix: fixed line calculator optimization

--- proj1/calculators/LineMandelCalculator.cc
+++ proj1/calculators/LineMandelCalculator.cc
@@ -1,8 +1,8 @@
 /**
  * @file LineMandelCalculator.cc
- * @author FULL NAME <xlogin00@stud.fit.vutbr.cz>
+ * @author Ondrej Lukasek (xlukas15@stud.fit.vutbr.cz)
  * @brief Implementation of Mandelbrot calculator that uses SIMD paralelization over lines
- * @date DATE
+ * @date 2024-10-20
  */
 #include <iostream>
 #include <string>
@@ -18,15 +18,46 @@
 LineMandelCalculator::LineMandelCalculator (unsigned matrixBaseSize, unsigned limit) :
 	BaseMandelCalculator(matrixBaseSize, limit, "LineMandelCalculator")
 {
-	// @TODO allocate & prefill memory
+	data = (int *)(malloc(height * width * sizeof(int)));
 }
 
 LineMandelCalculator::~LineMandelCalculator() {
-	// @TODO cleanup the memory
+	if (data != NULL) {
+		free(data);
+		data = NULL;
+	}
 }
 
 
 int * LineMandelCalculator::calculateMandelbrot () {
-	// @TODO implement the calculator & return array of integers
-	return NULL;
+	#pragma omp parallel for
+	for (int i = 0; i < height; i++) {
+		float y = y_start + i * dy;
+
+		#pragma omp simd
+		for (int j = 0; j < width; j++) {
+			float x = x_start + j * dx;
+
+			float zReal = x;
+			float zImag = y;
+			int value = limit;
+
+            for (int k = 0; k < limit; k++) {
+                float r2 = zReal * zReal;
+                float i2 = zImag * zImag;
+
+                if (r2 + i2 > 4.0f) {
+                    value = k;
+                    break;
+                }
+
+                zImag = 2.0f * zReal * zImag + y;
+                zReal = r2 - i2 + x;
+            }
+
+            data[i * width + j] = value;
+		}
+	}
+
+	return data;
 }
