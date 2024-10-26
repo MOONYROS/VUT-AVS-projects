0933cd4b23e96cc8ca1453607df22b17ebdc04f8
MOONYROS <ondrej@lukasek.cz>
2024-10-27 00:12:26 +0200

    !fix: batch calculator fixed

--- proj1/calculators/BatchMandelCalculator.cc
+++ proj1/calculators/BatchMandelCalculator.cc
@@ -18,7 +18,7 @@
 BatchMandelCalculator::BatchMandelCalculator (unsigned matrixBaseSize, unsigned limit) :
 	BaseMandelCalculator(matrixBaseSize, limit, "BatchMandelCalculator")
 {
-	data = (int *)(malloc(height * width * sizeof(int)));
+	data = (int *)(aligned_alloc(64, height * width * sizeof(int)));
 }
 
 BatchMandelCalculator::~BatchMandelCalculator() {
@@ -30,22 +30,29 @@ BatchMandelCalculator::~BatchMandelCalculator() {
 
 
 int * BatchMandelCalculator::calculateMandelbrot () {
-	// pojedeme pres vsechny radky po batchich
-	for (int batchStart = 0; batchStart < height; batchStart += batchSize) {
-		// kontrola konce -> bud jedu po batchSize nebo po max vysku
-		int batchEnd = std::min(batchStart + batchSize, height);
+	const int TILE_SIZE = 8;
 
-		// paralelizace -> kazdy radek batche je na jednom vlakne
-		#pragma omp parallel for
-		for (int i = batchStart; i < batchEnd; i++) {
+	// budeme postupovat po kusech matice
+	#pragma omp simd parallel for
+	for (int tileIndex = 0; tileIndex < (height * width); tileIndex += TILE_SIZE) {
+
+		// vypocet aktualni pozice v matici
+		int tileRowStart = (tileIndex / width) * TILE_SIZE;
+		int tileColStart = (tileIndex % width) / TILE_SIZE * TILE_SIZE;
+
+		// jedeme pres radky v ramci tilu nebo do konce matice
+		for (int i = tileRowStart; i < std::min(tileRowStart + TILE_SIZE, height); i++) {
 			float y = y_start + i * dy;
 
-			// vektorizace pro kazdy sloupec v radku v batchi
-			#pragma omp simd
-			for (int j = 0; j < width; j++) {
+			// budu pro radek pocitat uniky
+			int escapeCounter = 0;
+
+			// tady valime pres sloupce aktualniho tilu nebo do konce matice
+			#pragma omp simd aligned(data: 64) simdlen(8) reduction(+: escapeCounter)
+			for (int j = tileColStart; j < std::min(tileColStart + TILE_SIZE, width); j++) {
 				float x = x_start + j * dx;
 
-				// vypocet mandelbrotovy mnoziny
+				// zaciname pocitat mandelbrotovu mnozinu
 				float zReal = x;
 				float zImag = y;
 				int value = limit;
@@ -56,6 +63,7 @@ int * BatchMandelCalculator::calculateMandelbrot () {
 
 					if (r2 + i2 > 4.0f) {
 						value = k;
+						escapeCounter++; // mame unik -> inkrementujeme pocitadlo
 						break;
 					}
 
@@ -64,6 +72,11 @@ int * BatchMandelCalculator::calculateMandelbrot () {
 				}
 
 				data[i * width + j] = value;
+			}
+
+			// pokud vsechny body v radku unikly, nema smysl dal pocitat
+			if (escapeCounter >= width) {
+				break;
 			}
 		}
 	}
