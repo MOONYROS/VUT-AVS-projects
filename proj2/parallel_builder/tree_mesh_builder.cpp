ebc1ef6e0e572c894b6eb97ca47f2c70499207d5
MOONYROS <ondrej@lukasek.cz>
2024-11-25 21:57:05 +0100

    fix: date added to doxygen

--- proj2/parallel_builder/tree_mesh_builder.cpp
+++ proj2/parallel_builder/tree_mesh_builder.cpp
@@ -5,7 +5,7 @@
  *
  * @brief   Parallel Marching Cubes implementation using OpenMP tasks + octree early elimination
  *
- * @date    DATE
+ * @date    24.11.2024
  **/
 
 #include <iostream>
--- proj2/parallel_builder/tree_mesh_builder.h
+++ proj2/parallel_builder/tree_mesh_builder.h
@@ -5,7 +5,7 @@
  *
  * @brief   Parallel Marching Cubes implementation using OpenMP tasks + octree early elimination
  *
- * @date    DATE
+ * @date    24.11.2024
  **/
 
 #ifndef TREE_MESH_BUILDER_H
